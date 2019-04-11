8a1ba4bf620a0a470a292b142067e8aa253d7d9c
negabaro <negabaro@gmail.com>
2019-04-11 16:45:32 +0900

    add runtimechunk

---
+++ example2-3-SplitChunksPlugin-runtimeChunk/webpack.config.js
@@ -0,0 +1,22 @@
+var path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./app/index.js",
+    vendor: ["moment", "lodash"]
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist")
+  },
+  optimization: {
+    runtimeChunk: {
+      name: "runtime"
+    },
+    splitChunks: {
+      name: "vendor",
+      chunks: "initial"
+    }
+  }
+};
