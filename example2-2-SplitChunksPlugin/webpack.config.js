bb8bf1213c133faa73bf2dc57570192a806a8136
negabaro <negabaro@gmail.com>
2019-04-11 16:34:57 +0900

    add splitchunksplugin

---
+++ example2-2-SplitChunksPlugin/webpack.config.js
@@ -0,0 +1,19 @@
+var path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./app/index.js",
+    vendor: ["moment", "lodash"]
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist")
+  },
+  optimization: {
+    splitChunks: {
+      name: "vendor",
+      chunks: "initial"
+    }
+  }
+};
